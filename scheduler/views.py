d14a5e8cc39d2d55996040322a102ad5db3ce81d
“Mic” <mdaizovi@gmail.com>
2016-04-24 21:08:58 +0700

    another big view training

--- scheduler/views.py
+++ scheduler/views.py
@@ -129,6 +129,7 @@ def view_coach(request, coach_id):
 def view_training(request, activity_id):
     user=request.user
     single=False
+    visible=False
     try:
         training=Training.objects.get(pk=int(activity_id))
         rosters=[training.registered, training.auditing]
