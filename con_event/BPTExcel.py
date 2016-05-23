76d21c69983b99dc6f98dcd717ae7f6c4aaeaaf3
“Mic” <mdaizovi@gmail.com>
2016-05-23 23:22:00 +0200

    added sk98nanme AND number if only searching my ocn and email, when trying to find registrants for upload

--- con_event/BPTExcel.py
+++ con_event/BPTExcel.py
@@ -184,7 +184,7 @@ class BPTUploadForm(forms.Form):
                         this_reg=reg_q[0]
                         print "found %s, second try"%(this_reg)
                     else:#allow for same person, different email
-                        reg_q=Registrant.objects.filter(con=con).filter(Q(first_name__iexact=first_name,last_name__iexact=last_name)|Q(sk8name__iexact=sk8name))
+                        reg_q=Registrant.objects.filter(con=con).filter(Q(first_name__iexact=first_name,last_name__iexact=last_name)|Q(sk8name__iexact=sk8name,sk8number__iexact=sk8number))
                         if reg_q.count()==1:
                             this_reg=reg_q[0]
                             print "found %s, third try w/ diff email"%(this_reg)
