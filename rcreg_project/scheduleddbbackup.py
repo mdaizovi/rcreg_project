ad70188a75932685cc34250e43fd4af5f615cbbd
“Mic” <mdaizovi@gmail.com>
2016-06-13 15:09:20 +0200

    still testing db bckup

--- rcreg_project/scheduleddbbackup.py
+++ rcreg_project/scheduleddbbackup.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python
 import os
 import time
+from datetime import datetime
+
 #from https://www.pythonanywhere.com/forums/topic/119/
 # FYI, emergency reference: http://help.pythonanywhere.com/pages/MySQLBackupRestore/
 
@@ -14,16 +16,27 @@ DB_Name = 'rcregsite$default'
 DB_Host= db_User_Name+'.mysql.pythonanywhere-services.com'
 backupDir = '/home/rcregsite/backup'
 
-#datetime = time.strftime('%m%d%Y-%H%M%S')
-datetime = time.strftime('%Y %m %d')
-datetimeBackupDir = backupDir + "dbdump "+datetime
+now = datetime.now()
+print "now is ",now
+today=now.day
+print "today is ",today
 
-print "creating backup folder"
-if not os.path.exists(datetimeBackupDir):
-    os.makedirs(datetimeBackupDir)
+if int(today) in [13, 7, 21]:
 
-#original
-#mysqldump_cmd = "mysqldump -u " + db_User_Name + " --password='" + DB_User_Password + "' -h mysql.server --databases '" + DB_Name + "' > " + datetimeBackupDir + "/" + DB_Name + ".sql"
-#or:
-mysqldump_cmd = "mysqldump -u " + db_User_Name + " --password='" + DB_User_Password + "' -h "+DB_Host+" --databases '" + DB_Name + "' > " + datetimeBackupDir + "/" + DB_Name + ".sql"
-os.system(mysqldump_cmd)
+    #datetime = time.strftime('%m%d%Y-%H%M%S')
+    datetime = time.strftime('%Y %m %d')
+    datetimeBackupDir = backupDir + "/dbdump "+datetime
+
+    print "creating backup folder"
+    if not os.path.exists(datetimeBackupDir):
+        os.makedirs(datetimeBackupDir)
+
+    #original
+    #mysqldump_cmd = "mysqldump -u " + db_User_Name + " --password='" + DB_User_Password + "' -h mysql.server --databases '" + DB_Name + "' > " + datetimeBackupDir + "/" + DB_Name + ".sql"
+    #or:
+    mysqldump_cmd = "mysqldump -u " + db_User_Name + " --password='" + DB_User_Password + "' -h "+DB_Host+" --databases '" + DB_Name + "' > " + datetimeBackupDir + "/" + DB_Name + ".sql"
+    print "mysqldump_cmd ",mysqldump_cmd
+    #os.system(mysqldump_cmd)
+
+else:
+    print "No Scheduled DB backup today.\n"
