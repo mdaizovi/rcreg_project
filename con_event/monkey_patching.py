a498890b401ce617e4a95be51c76ec5dbd03be23
“Mic” <mdaizovi@gmail.com>
2016-06-14 21:36:04 +0200

    changed permission to blood rider can add registrant via upload, and changed monkey patch si is_a_boss includes blood rider but not any old staff member

--- con_event/monkey_patching.py
+++ con_event/monkey_patching.py
@@ -12,7 +12,9 @@ Sometimes models are imported in method to avoid import error."""
 
 
 def is_a_boss(self):
-    if self in list(User.objects.filter(Q(is_staff=True)|Q(groups__name__in=[BIG_BOSS_GROUP_NAME,LOWER_BOSS_GROUP_NAME]))):
+    #if self in list(User.objects.filter(Q(is_staff=True)|Q(groups__name__in=[BIG_BOSS_GROUP_NAME,LOWER_BOSS_GROUP_NAME]))):
+    #raising the bar for what it means to be a boss, seeing if that causes problems.
+    if self in list(User.objects.filter(Q(is_superuser=True)|Q(groups__name__in=[BIG_BOSS_GROUP_NAME,LOWER_BOSS_GROUP_NAME]))):
         return True
     else:
         return False
