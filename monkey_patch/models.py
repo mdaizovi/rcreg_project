43406772d4a75036b0c47c5929a5c11c3e9ac20b
RollerTron <rcregsite@gmail.com>
2016-09-03 22:34:51 +0200

    removing more potentially confusing junk

--- monkey_patch/models.py
+++ monkey_patch/models.py
@@ -1,11 +1,14 @@
 from django.contrib.auth.models import User
 from django.core.validators import MaxLengthValidator
 
-#reference: http://stackoverflow.com/questions/2610088/can-djangos-auth-user-username-be-varchar75-how-could-that-be-done
 
 NEW_USERNAME_LENGTH = 100
 
+
 def monkey_patch_username():
+    """Allows the username to be 100 chars, instead of just 30
+    for long emails, since email addy is username."""
+
     username = User._meta.get_field("username")
     username.max_length = NEW_USERNAME_LENGTH
     for v in username.validators:
