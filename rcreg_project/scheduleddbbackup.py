9df1abbc3eda787b4ea770133286523fd5cbfac3
RollerTron <rcregsite@gmail.com>
2016-09-03 19:28:02 +0200

    made scheduleddbbackup pep8 compliant

--- rcreg_project/scheduleddbbackup.py
+++ rcreg_project/scheduleddbbackup.py
@@ -3,37 +3,30 @@ import os
 import time
 from datetime import datetime
 
-#from https://www.pythonanywhere.com/forums/topic/119/
-# FYI, emergency reference: http://help.pythonanywhere.com/pages/MySQLBackupRestore/
-
-
-############Production
-# #mysqldump -u rcregsite -p -h rcregsite.mysql.pythonanywhere-services.com --databases 'rcregsite$default' > /home/rcregsite/dump.sql
-
 db_User_Name = 'rcregsite'
-DB_User_Password = 'mice4rice'
+DB_User_Password = 'TRON<3s5318008'
 DB_Name = 'rcregsite$default'
-DB_Host= db_User_Name+'.mysql.pythonanywhere-services.com'
+DB_Host = db_User_Name+'.mysql.pythonanywhere-services.com'
 backupDir = '/home/rcregsite/backup/'
 
 now = datetime.now()
-today=now.day
-print "today is ",today
+today = now.day
 
 if int(today) in [7, 21]:
 
     datetime = time.strftime('%Y %m %d')
     datetimeBackupDir = backupDir + datetime+"/"
 
-    print "finding backup folder"
     if not os.path.exists(datetimeBackupDir):
-        print "creating backup folder"
         os.makedirs(datetimeBackupDir)
 
-    mysqldump_cmd = "mysqldump -u " + db_User_Name + " --password='" + DB_User_Password + "' -h "+DB_Host+" --databases '" + DB_Name + "' > '" + datetimeBackupDir +DB_Name + ".sql'"
-    #print "mysqldump_cmd ",mysqldump_cmd
+    mysqldump_cmd = "mysqldump -u " + db_User_Name + " --password='"
+    + DB_User_Password + "' -h " + DB_Host + " --databases '" + DB_Name
+    + "' > '" + datetimeBackupDir + DB_Name + ".sql'"
+
     os.system(mysqldump_cmd)
-    print "db dumped"
+    print "db copied"
 
 else:
-    print "No Scheduled DB backup today.\n"
+    print "No Scheduled DB backup today."
+    
